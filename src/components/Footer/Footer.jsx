2f423371ea6ba706085a70cb4cd81606a0b1c42d
Md Kamran Taslim <54260472+KamranTaslim@users.noreply.github.com>
2025-03-23 19:02:55 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -29,7 +29,7 @@ function Footer() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/send-email", {
+      const response = await fetch("https://iengineeringbackend.onrender.com/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
